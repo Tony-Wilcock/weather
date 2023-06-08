2ddb89012a7b529cfca7f45e4e8852449a078aa7
Tony Wilcock <tonywilcock80@gmail.com>
2023-06-08 18:54:22 +0100

    Added Open-meteo API

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from '@/apollo-client';
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+
 type Props = {
   params: {
     city: string;
@@ -6,12 +9,41 @@ type Props = {
   };
 };
 
-const WeatherPage = ({ params: { city, lat, long } }: Props) => {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: 'true',
+      longitude: long,
+      latitude: lat,
+      timezone: 'GMT',
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
   return (
     <div>
-      WeatherPage: {city} {lat} {long}
+      {/* <InfoPanel /> */}
+
+      <div>
+        <div>
+          <div>
+            <h2>Todays Overview</h2>
+            <p>
+              Last Updated at:{' '}
+              {new Date(results.current_weather.time).toLocaleString()} (
+              {results.timezone})
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default WeatherPage;
